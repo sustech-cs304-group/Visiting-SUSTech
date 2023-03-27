e523be44cb043a67152add70217c0d887201e121
sui_h <12012929@mail.sustech.edu.cn>
2023-03-27 17:50:37 +0800

    Add springboot template

---
+++ src/main/java/com/cs304/visitingsustech/VisitingSusTechApplication.java
@@ -0,0 +1,13 @@
+package com.cs304.visitingsustech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitingSusTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisitingSusTechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cs304/visitingsustech/VisitingSusTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs304.visitingsustech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitingSusTechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
