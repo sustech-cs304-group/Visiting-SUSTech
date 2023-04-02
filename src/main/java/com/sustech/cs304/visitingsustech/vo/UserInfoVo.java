4c2c32a29e047b589441ea40e5105e69c578e5fc
sui_h <78638687+He-Sui@users.noreply.github.com>
2023-04-02 11:30:10 +0800

    Add nickname, avatar url to DDL.

---
+++ src/main/java/com/sustech/cs304/visitingsustech/vo/UserInfoVo.java
@@ -0,0 +1,12 @@
+package com.sustech.cs304.visitingsustech.vo;
+
+import lombok.Data;
+
+@Data
+public class UserInfoVo {
+    private String name;
+    private String phone;
+    private String identityCard;
+    private String type;
+    private Integer gender;
+}
