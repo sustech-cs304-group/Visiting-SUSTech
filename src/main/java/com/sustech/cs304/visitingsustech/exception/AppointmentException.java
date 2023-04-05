d006a4d1eb9748bc64b618b80e7013583512b5f5
sui_h <78638687+He-Sui@users.noreply.github.com>
2023-04-05 11:10:16 +0800

    Add Exceptions.

---
+++ src/main/java/com/sustech/cs304/visitingsustech/exception/AppointmentException.java
@@ -0,0 +1,19 @@
+package com.sustech.cs304.visitingsustech.exception;
+
+public class AppointmentException extends BaseException{
+    public AppointmentException() {
+        super("Bad Request", 400);
+    }
+
+    public AppointmentException(String message) {
+        super(message, 400);
+    }
+
+    public AppointmentException(int status) {
+        super("Bad Request", status);
+    }
+
+    public AppointmentException(String message, int status) {
+        super(message, status);
+    }
+}
---
+++ src/main/java/com/sustech/cs304/visitingsustech/exception/BaseException.java
@@ -0,0 +1,29 @@
+package com.sustech.cs304.visitingsustech.exception;
+
+public class BaseException extends RuntimeException {
+    private final int status;
+
+    public BaseException() {
+        super();
+        this.status = 500;
+    }
+
+    public BaseException(String message, int status) {
+        super(message);
+        this.status = status;
+    }
+
+    public BaseException(int status) {
+        super();
+        this.status = status;
+    }
+
+    public BaseException(String message) {
+        super(message);
+        this.status = 500;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+}
