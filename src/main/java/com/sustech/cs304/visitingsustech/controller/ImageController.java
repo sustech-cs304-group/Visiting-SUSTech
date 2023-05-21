93f67c2ae19e190d3395ef1fba4ba6808d73208d
sui_h <78638687+He-Sui@users.noreply.github.com>
2023-05-21 21:48:23 +0800

    add try catch on user authentication

--- src/main/java/com/sustech/cs304/visitingsustech/controller/ImageController.java
+++ src/main/java/com/sustech/cs304/visitingsustech/controller/ImageController.java
@@ -5,7 +5,9 @@ import com.sustech.cs304.visitingsustech.exception.BaseException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
